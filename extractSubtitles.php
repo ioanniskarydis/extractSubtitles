1a37d3d18e857d8324a675fecf374ef6a4410684
Ioannis Karydis <71339657+ioanniskarydis@users.noreply.github.com>
2021-06-02 19:59:19 +0300

    Update extractSubtitles.php

--- extractSubtitles.php
+++ extractSubtitles.php
@@ -7,7 +7,7 @@
 		--ext 		parameter optional, if set requires value: the extension (and implicitly the format) of the subtitle to export, default value is srt
 		--language	parameter optional, if set requires value: extract the subtitles with language definition equal to value of argument
 		--title		parameter optional, if set requires value: extract the subtitles with title definition equal to value of argument
-		--auto		parameter optional, accepts no value: attempts to extract the subtitles with title='English [SDH]', else with 	title='English', else with title='Greek', else asks the user (change titles and ordering at the $defaultTitles variable
+		--auto		parameter optional, accepts no value: attempts to extract the subtitles with title from values and ordering of the $defaultTitles variable, then, if none such title value exists, attempts to extract the subtitles with language from values and ordering of the $defaultLanguages variable, then, if none such language value exists, asks the user
 		--overwrite	parameter optional, accepts no value: if the output file exists, then auto overwrite, otherwise user's verification is required
 		
 		e.g.: 	php extractSubtitles.php -v -i aMovie.mp4 --language=ita
@@ -19,13 +19,15 @@
 	
 	//Check ffmpeg && ffprobe are available
 	if (command_exists("ffmpeg") === false || command_exists("ffprobe") === false)
-		die ("\033[31mThis script requires ffmpeg & ffprobe accessible by their respective commands which seem to not be available, exiting.\n");
+		die ("\033[31mThis script requires ffmpeg & ffprobe accessible by their respective commands which seem to not be available, exiting.\033[0m\n");
 	
 	//Get the parameters
 	$options = getopt("vi:o:", array("language:", "title:", "auto", "ext:", "overwrite"));
 	
 	//Default title ordering for --auto
 	$defaultTitles = array('English [SDH]', 'English', 'Greek');
+	//Default language ordering for --auto
+	$defaultLanguages = array('eng', 'gre');
 	
 	//Make execution verbose
 	$verbose = false;
@@ -34,9 +36,9 @@
 	
 	//Check -i is set and the file to operate on exists
 	if (isset($options['i']) === false)
-		die ("\033[31mNo input, exiting.\n");
+		die ("\033[31mNo input, exiting.\033[0m\n");
 	elseif (file_exists($options['i']) === false)
-		die ("\033[31mInput file not found, exiting.\n");
+		die ("\033[31mInput file not found, exiting.\033[0m\n");
 	else {
 		
 		//Split the filename to parts in order to change name the subtitle to export
@@ -61,12 +63,12 @@
 		$reply = null;
 		$overwrite = false;
 		while ($reply === null && file_exists($subtitleFilename) && isset($options['overwrite']) !== true) {
-			$reply = readline('File already exists. Overwrite? [y/N] ');
+			$reply = readline('File \''.$subtitleFilename.'\' already exists. Overwrite? [y/N] ');
 			
 			if (strcasecmp($reply, 'y') === 0)
 				$overwrite = true;
 			elseif (strcasecmp($reply, '') === 0 || strcasecmp($reply, 'n') === 0)
-				die("\033[31mFile exists, overwrite was not allowed, exiting.\r\n");
+				die("\033[31mFile '$subtitleFilename' exists, overwrite was not allowed, exiting.\033[0m\r\n");
 			else
 				$reply = null;
 		}
@@ -85,7 +87,7 @@
 		
 		//In case the ffprobe did not execute correctly
 		if ($result_code === 1)
-			die ("\033[31mResult_code of ffprobe was: $result_code, exiting.\n");
+			die ("\033[31mResult_code of ffprobe was: $result_code, exiting.\033[0m\n");
 		
 		//Combine the result from array to string
 		$allSubtitles = implode($allSubtitles, "\r\n");
@@ -95,12 +97,12 @@
 			$allSubtitles = json_decode($allSubtitles, true);
 		} catch (Exception $e) {
 			echo 'Caught exception: ',  $e->getMessage(), "\n";
-			die ("\033[31mThis is not a JSON file\n");
+			die ("\033[31mThis is not a JSON file.\033[0m\n");
 		}
 		
-		//In case there was an error in the convertion
+		//In case there was an error in the conversion
 		if ($allSubtitles === null)
-			die ("\033[31mResult_code of ffprobe was not JSON\n");
+			die ("\033[31mResult_code of ffprobe was not JSON.\033[0m\n");
 		
 		//Returned assoc array has some extra fields
 		$allSubtitles = $allSubtitles['streams'];
@@ -116,40 +118,50 @@
 		$extractSubtitleCommand = null;
 		
 		//Get all available titles in an array
-		$allTitles = array_map(function($element){return $element['tags']['title'];}, $allSubtitles);
+		$allTitles = array_map(function($element){if (isset($element['tags']['title'])) return $element['tags']['title'];}, $allSubtitles);
 		
+		//Get all available languages
+		$allLanguages = array_map(function($element){if (isset($element['tags']['language'])) return $element['tags']['language'];}, $allSubtitles);
 		
 		if ($noOfAllSubtitles === 0) {	//If the input file has no sutitles
-			die("\033[31mThe file '".$options['i']."' has no embedded subtitles, exiting.\r\n");
+			die("\033[31mThe file '".$options['i']."' has no embedded subtitles, exiting.\033[0m\r\n");
 		}
 		elseif ($noOfAllSubtitles === 1) {	//if only one subtitle exists in the input file, extract it no questions asked
 			$extractSubtitleCommand = 'ffmpeg -i "'.$options['i'].'" -map 0:s:0 "'.$subtitleFilename.'"';
 		} elseif (isset($options['language']) === true) {	//If the --language is set
-			
-			//Get all available languages
-			$allLanguages = array_map(function($element){return $element['tags']['language'];}, $allSubtitles);
-			
+						
 			if (in_array($options['language'], $allLanguages))
 				$extractSubtitleCommand = 'ffmpeg -i "'.$options['i'].'" -map 0:s:m:language:"'.$options['language'].'" "'.$subtitleFilename.'"';
 			else
-				die("\033[31mLanguage '".$options["language"]."' does not exist in the input file, exiting.\r\n");
+				die("\033[31mLanguage '".$options["language"]."' does not exist in the input file, exiting.\033[0m\r\n");
 		} elseif (isset($options['title']) === true) {	//If the --title is set
-			if (in_array($options['title'], $allTitles))
+			if (is_array($allTitles) && in_array($options['title'], $allTitles))
 				$extractSubtitleCommand = 'ffmpeg -i "'.$options['i'].'" -map 0:s:m:title:"'.$options['title'].'" "'.$subtitleFilename.'"';
 			else
-				die("\033[31mTitle '".$options['title']."' does not exist in the input file, exiting.\r\n");
+				die("\033[31mTitle '".$options['title']."' does not exist in the input file, exiting.\033[0m\r\n");
 		} else {
 			//If the --auto is set
 			if (isset($options['auto']) === true){	
 				
 				//Select a subtitle based on title and the $defaultTitles array's values (ordered)
 				foreach ($defaultTitles as $aDefaultTitle) {
-					if (in_array($aDefaultTitle, $allTitles)) {
+					if (is_array($allTitles) && in_array($aDefaultTitle, $allTitles)) {
 						$extractSubtitleCommand = 'ffmpeg -i "'.$options['i'].'" -map 0:s:m:title:"'.$aDefaultTitle.'" "'.$subtitleFilename.'"';
 						
 						break;
 					}
 				} 
+				
+				if ($extractSubtitleCommand === null) {
+					//Select a subtitle based on language and the $defaultLanguages array's values (ordered)
+					foreach ($defaultLanguages as $aDefaultLanguage) {
+						if (is_array($allLanguages) && in_array($aDefaultLanguage, $allLanguages)) {
+							$extractSubtitleCommand = 'ffmpeg -i "'.$options['i'].'" -map 0:s:m:language:"'.$aDefaultLanguage.'" "'.$subtitleFilename.'"';
+							
+							break;
+						}
+					} 
+				}
 			} 
 			
 			//If still there's no selection of what subtitle to extract
@@ -157,7 +169,7 @@
 				
 				//Show the alternatives
 				foreach($allSubtitles as $key => $aSubtitle)
-					echo "\t".($key+1)." Title: ".$aSubtitle['tags']['title']." (language: ".$aSubtitle['tags']['language'].")\r\n";
+					echo "\t".($key+1)." Title: ".(isset($aSubtitle['tags']['title']) ? $aSubtitle['tags']['title'] : "<N/A>")." (language: ".$aSubtitle['tags']['language'].")\r\n";
 				
 				//As long as the user has not selected a valid id
 				do {
@@ -187,7 +199,7 @@
 		
 		//In case there's an error
 		if ($result_code === 1)
-			die ("\033[31mResult_code of ffmpeg was: $result_code\n");
+			die ("\033[31mResult_code of ffmpeg was: $result_code.\033[0m\n");
 		
 	}
 	
